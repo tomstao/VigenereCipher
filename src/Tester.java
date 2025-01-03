faeebdaf70d92fe41ac1980618b5849cf0d3b5fe
tomstao <96sutao@gmail.com>
2025-01-02 22:42:16 -0500

    Test methods for CaesarCipher and CaesarCracker completed.

--- src/Tester.java
+++ src/Tester.java
@@ -1,2 +1,33 @@
+import edu.duke.FileResource;
+
 public class Tester {
+
+    public void testCaserCipher() {
+        CaesarCipher cc = new CaesarCipher(4);
+
+        FileResource fr = new FileResource("VigenereTestData/titus-small.txt");
+
+        String encryptedStr = cc.encrypt(fr.asString());
+        System.out.println("Encrypted: " + encryptedStr);
+
+        String decryptedStr = cc.decrypt(encryptedStr);
+        System.out.println("Decrypted: " + decryptedStr);
+
+    }
+
+
+    public void testCaesarCracker() {
+
+        CaesarCracker cc = new CaesarCracker();
+        FileResource fr = new FileResource("VigenereTestData/titus-small_key5.txt");
+
+        String decryptedStr = cc.decrypt(fr.asString());
+        System.out.println("Decrypted: " + decryptedStr);
+
+        CaesarCracker cc2 = new CaesarCracker('a');
+        fr = new FileResource("VigenereTestData/oslusiadas_key17.txt");
+        decryptedStr = cc2.decrypt(fr.asString());
+        System.out.println("Decrypted 2: " + decryptedStr);
+    }
+
 }
